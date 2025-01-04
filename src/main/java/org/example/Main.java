9f471de55030359c2ea2a2c287f32604199ba531
Wojciech Dominiak <wojciechdominiak80@gmail.com>
2025-01-04 14:58:38 +0100

    work with scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,28 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Podaj ścieżkę do pliku PDF: ");
+        String inputFilePath = scanner.nextLine();
+
+        System.out.print("Podaj ścieżkę do nowego pliku PDF: ");
+        String outputFilePath = scanner.nextLine();
+
+        System.out.print("Podaj numery stron do wyodrębnienia (np. 1,3,5): ");
+        String pagesInput = scanner.nextLine();
+
+        PDFPageExtractor extractor = new PDFPageExtractor();
+
+        try {
+            extractor.extractPages(inputFilePath, outputFilePath, extractor.parsePageNumbers(pagesInput));
+            System.out.println("Wybrane strony zostały zapisane do: " + outputFilePath);
+        } catch (IOException e) {
+            System.err.println("Wystąpił błąd: " + e.getMessage());
+        }
     }
 }
